a172011facf70aabd420eef2ef5587be39cb1dd2
lat9 <lat9@vinosdefrutastropicales.com>
2014-02-03 07:46:07 -0600

    Issue #8:  Add language constant
    Include a language constant (and some copyright information) to the
    "goodbye" script.

--- includes/modules/pages/checkout_multiship/jscript_goodbye.php
+++ includes/modules/pages/checkout_multiship/jscript_goodbye.php
@@ -1,4 +1,11 @@
 <?php
+// ---------------------------------------------------------------------------
+// Part of the Multiple Shipping Addresses plugin for Zen Cart v1.5.1 and later
+//
+// Copyright (C) 2014, Vinos de Frutas Tropicales (lat9)
+//
+// @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+// ---------------------------------------------------------------------------
 ?>
 <script type="text/javascript">
 var canleave = true;
@@ -14,7 +21,7 @@ function goodbye(e) {
   if (!canleave) {
     //e.cancelBubble is supported by IE - this will kill the bubbling process.
     e.cancelBubble = true;
-    e.returnValue = 'You sure you want to leave?'; //This is displayed on the dialog
+    e.returnValue = '<?php echo TEXT_QUANTITIES_CHANGED; ?>'; // This is displayed on the dialog
 
     //e.stopPropagation works in Firefox.
     if (e.stopPropagation) {
