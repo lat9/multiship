3b7e36860afc5a3dc1801e35344c18ceea198a8c
lat9 <lat9@vinosdefrutastropicales.com>
2017-07-25 07:59:14 -0400

    #27: Correct partial-uninstall issue
    Update the SQL uninstall script to remove all remnants of the Multiship
    plugin!

--- uninstall_multiship.sql
+++ uninstall_multiship.sql
@@ -1,3 +1,5 @@
 DROP TABLE orders_multiship;
 DROP TABLE orders_multiship_total;
 ALTER TABLE orders_products DROP orders_multiship_id;
+DELETE FROM configuration WHERE configuration_key LIKE 'MODULE_MULTISHIP_%';
+DELETE FROM admin_pages WHERE page_key IN ('customersInvoiceMultiship', 'customersPackingslipMultiship');
